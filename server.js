4d2484a9892e4a2128fd301519905d6366d78312
Patrick Egan <mrpatrickegan@gmail.com>
2016-12-01 22:34:16 +0000

    final commit

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express')
 var app = express()
-app.get('/', function(req,res) {
-    res.send('Hello World!')
+app.get('/:time', function(req,res) {
+    var time = req.params.time
+    var unix, normal;
+    var parsedDate = Date.parse(time);
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (Number.isInteger(parseInt(time))) {
+        unix = new Date(parseInt(time))
+        normal = months[unix.getMonth()] + " " + unix.getDate() + ", " + unix.getFullYear();
+    }
+    else if (Number.isInteger(parsedDate)) {
+        normal = time
+        unix = new Date(Date.parse(time))
+    }
+    else {
+        normal = null;
+        unix = null;
+    }
+    res.json({unix: (unix==null)?unix:unix.getTime(), natural: normal});
 })
 
 app.listen(8080, function() {
